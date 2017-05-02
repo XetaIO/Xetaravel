f13768ee1cd54c4f7acf5e04253479cc9f445968
Xety <emeric@xeta.io>
2017-05-02 22:02:55 +0200

    test fix

--- database/migrations/2017_05_02_133801_create_media_table.php
+++ database/migrations/2017_05_02_133801_create_media_table.php
@@ -12,7 +12,7 @@ class CreateMediaTable extends Migration
     public function up()
     {
         Schema::create('media', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
+            $table->increments('id');
             $table->morphs('model');
             $table->string('collection_name');
             $table->string('name');
@@ -20,8 +20,8 @@ class CreateMediaTable extends Migration
             $table->string('mime_type')->nullable();
             $table->string('disk');
             $table->unsignedInteger('size');
-            $table->json('manipulations')->nullable();
-            $table->json('custom_properties')->nullable();
+            $table->json('manipulations');
+            $table->json('custom_properties');
             $table->unsignedInteger('order_column')->nullable();
             $table->nullableTimestamps();
         });
