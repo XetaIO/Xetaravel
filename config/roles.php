c186fa8cc29579bb91a77e86889a478d3172a21e
Xety <emeric@xeta.io>
2017-05-23 03:16:23 +0200

    Rolled back the fix

--- config/roles.php
+++ config/roles.php
@@ -4,18 +4,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Package Connection
-    |--------------------------------------------------------------------------
-    |
-    | You can set a different database connection for this package. It will set
-    | new connection for models Role and Permission. When this option is null,
-    | it will connect to the main database, which is set up in database.php
-    |
-    */
-    'connection' => null,
-
-    /*
-    |--------------------------------------------------------------------------
     | Slug Separator
     |--------------------------------------------------------------------------
     |
