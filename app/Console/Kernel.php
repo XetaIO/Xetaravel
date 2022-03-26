31892212cc8e4beba99eb1dcbdfb91a34be45a1d
Xety <zoro.fmt@gmail.com>
2022-03-26 11:05:05 +0100

    Switch everyMinute to weekly schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('leaderboard:update')
-                ->everyMinute()
+                ->weekly()
                 ->runInBackground();
                 //->appendOutputTo('/home/vagrant/xetaravel/scheduler.log');
     }
