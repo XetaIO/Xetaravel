f82e358b7ae83a08976232faa4c6c5736e226adb
Xety <zoro.fmt@gmail.com>
2022-04-22 17:22:08 +0200

    Update admin footer

--- resources/views/Admin/elements/footer.blade.php
+++ resources/views/Admin/elements/footer.blade.php
@@ -2,18 +2,33 @@
   <div class="container-fluid">
       <div class="row">
         <div class="col-md-6">
-              &copy; {{ date('Y', time()) }} {{ config('app.name') }}.
-          <ul class="list-inline d-inline-block mb-0">
-            <li class="list-inline-item">
-              <a href="{{ config('xetaravel.site.github_url') }}" target="_blank">
-                <i class="fa fa-github-alt" data-toggle="tooltip" title="Source Code available on Github"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
+			&copy; {{ date('Y', time()) }} {{ config('app.name') }}
+			<ul class="list-inline d-inline-block mb-0 ml-1">
+				<li class="list-inline-item">
+					<a href="{{ route('page.terms') }}">
+						Terms
+					</a>
+				</li>
+				<li class="list-inline-item">
+					<a href="{{ route('page.contact') }}">
+						Contact
+					</a>
+				</li>
+				<li class="list-inline-item">
+					<a href="{{ config('xetaravel.site.github_url') }}" target="_blank">
+						<i class="fa fa-github-alt" data-toggle="tooltip" title="Source Code available on Github"></i>
+					</a>
+				</li>
+				@if (config('xetaravel.version'))
+					<li class="list-inline-item">
+						<small>Version : {{ config('xetaravel.version') }}</small>
+					</li>
+				@endif
+			</ul>
+		</div>
 
         <div class="col-md-6 text-md-right">
-          <i class="fa fa-code text-primary" style="font-weight: bold;"></i> with <i class="fa fa-heart" style="color: #fa6c65"></i> and <i class="fa fa-coffee" style="color: #826644"></i> by <a href="https://github.com/Xety" target="_blank">@Xeta</a>
+          <i class="fa fa-code text-primary" style="font-weight: bold;"></i> with <i class="fa fa-heart" style="color: #fa6c65"></i> and <i class="fa fa-coffee" style="color: #826644"></i> by <a href="https://github.com/Xety" target="_blank">@Emeric</a>
         </div>
     </div>
   </div>
