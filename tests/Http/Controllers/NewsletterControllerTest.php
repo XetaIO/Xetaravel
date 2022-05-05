aecb7c3e309f9b44d24d4814a7c6c26c84458601
Emeric Fèvre <emeric@xetaravel.com>
2022-05-05 23:04:37 +0200

    Add Unit Tests

---
+++ tests/Http/Controllers/NewsletterControllerTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Tests\Http\Controllers;
+
+use Xetaravel\Models\Newsletter;
+use Tests\TestCase;
+
+class NewsletterControllerTest extends TestCase
+{
+    /**
+     * testSubscribeSuccess method
+     *
+     * @return void
+     */
+    public function testSubscribeSuccess()
+    {
+        $data = [
+            'email' => 'newsletter@xetaravel.io'
+        ];
+        $response = $this->post('/newsletter/subscribe', $data);
+        $response->assertRedirect();
+
+        $newsletter = Newsletter::where('email', $data['email'])->first();
+        $this->assertSame($newsletter->email, $data['email']);
+    }
+
+    /**
+     * testSubscribeAlreadyExist method
+     *
+     * @return void
+     */
+    public function testSubscribeAlreadyExist()
+    {
+        $data = [
+            'email' => 'newsletter@xetaravel.io'
+        ];
+        $response = $this->post('/newsletter/subscribe', $data);
+        $response->assertRedirect();
+        $response->assertSessionHas('success');
+
+        $response = $this->post('/newsletter/subscribe', $data);
+        $response->assertSessionHas('danger');
+    }
+
+    /**
+     * testUnsubscribe method
+     *
+     * @return void
+     */
+    public function testUnsubscribe()
+    {
+        $data = [
+            'email' => 'newsletter@xetaravel.io'
+        ];
+        $response = $this->post('/newsletter/subscribe', $data);
+        $response->assertRedirect();
+        $response->assertSessionHas('success');
+
+        $response = $this->get('/newsletter/unsubscribe/' . $data['email']);
+        $response->assertRedirect();
+        $response->assertSessionHas('success');
+    }
+}
