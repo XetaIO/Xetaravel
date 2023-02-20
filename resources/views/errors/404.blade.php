cd12aacb6d4cc9ca654521f9f060c91b00eda690
Emeric Fevre <emeric@xetaravel.com>
2023-02-20 19:06:37 +0100

    Remove error layout and reworked errors pages : 404, 500 & 503

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.error')
+@extends('layouts.app')
 {!! config(['app.title' => 'Not found']) !!}
 
 @push('meta')
@@ -6,22 +6,21 @@
 @endpush
 
 @section('content')
-<div class="container mt-4 pb-4">
-    <div class="error">
-        <div class="title font-xeta">
+<div class="lg:container mx-auto mt-12 mb-5">
+    <div class="text-center">
+        <h1 class="text-[14rem] font-xetaravel font-bold text-error mb-7">
             404
-        </div>
-        <div class="description mb-1">
+        </h1>
+        <div class="text-xl mb-12">
             The page you're looking for doesn't exist.
         </div>
-        <div class="link">
-            {!! link_to(
-                route('page.index'),
-                '<i class="fa fa-home" aria-hidden="true"></i> Accueil',
-                ['class' => 'btn btn-outline-primary'],
-                true,
-                false
-            ) !!}
+        <div class="mb-8">
+            <a class="btn btn-primary  gap-2" href="{{ route('page.index') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                </svg>
+                Home
+            </a>
         </div>
     </div>
 </div>
