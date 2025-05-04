a048135430f3743a1af9c8834d465a6a7acf523e
Xety <8210023+Xety@users.noreply.github.com>
2025-05-04 10:35:37 +0000

    Apply automatic changes

--- database/factories/BadgeFactory.php
+++ database/factories/BadgeFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use Xetaravel\Models\Badge;
 
 class BadgeFactory extends Factory
--- database/migrations/2025_03_11_204547_create_media_table.php
+++ database/migrations/2025_03_11_204547_create_media_table.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-
     public function up(): void
     {
         Schema::create('media', function (Blueprint $table) {
